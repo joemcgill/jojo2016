549613a59d51a3a1f10d68f1dc2efc8286aa09ba
Joe McGill <joemcgill@gmail.com>
2016-03-05 21:08:37 -0600

    Document template for social icon widget

---
+++ template-parts/icons.php
@@ -0,0 +1,9 @@
+<div class="social-icons">
+	<ul class="icon-list list-inline">
+		<li class="list-item"><a href="https://www.facebook.com/jojotatic/"><div class="icon icon-facebook"><span class="icon-text">facebook</span></div></a></li>
+		<li class="list-item"><a href="https://twitter.com/jojotatic/"><div class="icon icon-twitter"><span class="icon-text">twitter</span></div></a></li>
+		<li class="list-item"><a href="https://www.instagram.com/jojotatic/"><div class="icon icon-instagram"><span class="icon-text">instagram</span></div></a></li>
+		<li class="list-item"><a href="https://www.youtube.com/jojotatic/"><div class="icon icon-youtube"><span class="icon-text">youtube</span></div></a></li>
+		<li class="list-item"><a href="https://www.snapchat.com/add/jojotatic/"><div class="icon icon-snapchat"><span class="icon-text">snapchat</span></div></a></li>
+	</ul>
+</div>
