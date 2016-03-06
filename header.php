bda5f1b2f8eafb1dc13c23d2aaec15834e05c9f2
Joe McGill <joemcgill@gmail.com>
2016-03-05 22:31:08 -0600

    Header: Mobile styles and prep menu toggle
    * Updated paths to JS scripts
    * Adds icon_menu.svg
    * Updates navigation.js to use new menu toggle

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jojo2016' ); ?></button>
+			<div class="menu-toggle icon-menu" aria-controls="primary-menu" aria-expanded="false"><span class="screen-reader-text"><?php esc_html_e( 'Primary Menu', 'jojo2016' ); ?></span></div>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
