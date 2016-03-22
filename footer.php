674117b180457a5c8c0d5810d7e6f6f8e359fc57
Joe McGill <joemcgill@gmail.com>
2016-03-21 21:54:35 -0500

    Update footer with copyright and disclosures

--- footer.php
+++ footer.php
@@ -22,7 +22,8 @@
 		</div>
 		<div class="site-footer-meta">
 			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jojo2016' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'jojo2016' ), 'WordPress' ); ?></a>
+				<?php printf( esc_html__( '&copy; %d Jojotastic,', 'jojo2016' ), date( 'Y' ) ); ?>
+				<a href="/policy">Terms &amp; Disclosures</a>
 				<span class="sep"> | </span>
 				<?php printf( esc_html__( 'Theme design by: %1$s. Site development by: %2$s.', 'jojo2016' ), '<a href="http://ashsmash.com/" rel="designer">Ash Huang</a>', '<a href="http://joemcgill.net/" rel="developer">Joe McGill</a>' ); ?>
 			</div><!-- .site-info -->
