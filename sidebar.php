e4f140e84557dd18e92c0658e9f7e475ffbf9adf
Joe McGill <joemcgill@gmail.com>
2016-05-24 22:19:58 -0500

    Remove video thumbnails in the sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,8 +10,14 @@
 if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
-?>
 
+// Disable the Featured Video Plus filter on sidebar items.
+global $featured_video_plus;
+if ( ! is_null( $featured_video_plus ) ) {
+	remove_filter( 'post_thumbnail_html', array( $featured_video_plus, 'filter_post_thumbnail' ), 99, 5 );
+}
+
+?>
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
