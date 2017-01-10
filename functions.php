1a5465d61391af7883d593eb798cbe8f420f2332
Joe McGill <joemcgill@gmail.com>
2017-01-09 20:23:08 -0600

    Fix incorrect function name

--- functions.php
+++ functions.php
@@ -277,7 +277,7 @@ add_action( 'init', 'jojo2016_force_canonical_redirects' );
 function jojo2016_nonce_life() {
 	return 60 * 60 * 24 * 30; // 30 days.
 }
-add_filter( 'ppp_nonce_life', 'jojo_nonce_life' );
+add_filter( 'ppp_nonce_life', 'jojo2016_nonce_life' );
 
 /**
  * Implement the Custom Header feature.
